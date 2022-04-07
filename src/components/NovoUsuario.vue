e6e1e1938300305c038aa29e355904e095161c86
Jhonny Ribeiro <jaribeiro32@gmail.com>
2022-04-07 04:32:36 -0300

    add form

--- src/components/NovoUsuario.vue
+++ src/components/NovoUsuario.vue
@@ -1,3 +1,58 @@
 <template>
-  <h1>Novo Usuário</h1>
+  <div class="container">
+    <h1>Novo Usuário</h1>
+    <form @submit.prevent.stop="enviarFormulario()">
+      <div class="from-group">
+        <label for="nome">Nome</label>
+        <input
+          type="text"
+          class="form-control"
+          v-model="usuario.nome"
+          id="nome"
+        />
+      </div>
+
+      <div class="from-group">
+        <label for="email">E-mail</label>
+        <input
+          type="email"
+          class="form-control"
+          v-model="usuario.email"
+          id="email"
+        />
+      </div>
+
+      <div class="from-group">
+        <label for="senha">Senha</label>
+        <input
+          type="password"
+          class="form-control"
+          v-model="usuario.password"
+          id="senha"
+        />
+      </div>
+
+      <button type="submit" class="btn btn-success mt-2">Salvar</button>
+    </form>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      usuario: {
+        nome: "",
+        email: "",
+        password: "",
+      },
+    };
+  },
+
+  methods: {
+    enviarFormulario() {
+      console.log(this.usuario);
+    },
+  },
+};
+</script>
