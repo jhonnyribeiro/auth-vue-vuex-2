9d0c8cfa6e5ba0a0f7eb28a8a82788eebe182e4b
Jhonny Ribeiro <jaribeiro32@gmail.com>
2022-04-07 04:23:21 -0300

    New user component

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,31 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Gerentes from '../views/Gerentes.vue'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Gerentes from "../views/Gerentes.vue";
+import Home from "../views/Home.vue";
+import NovoUsuario from "../components/NovoUsuario";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '',
-    name: 'home',
-    component: Home
+    path: "",
+    name: "home",
+    component: Home,
   },
   {
-    path: '/gerentes',
-    name: 'gerentes',
-    component: Gerentes
-  }
-]
+    path: "/gerentes",
+    name: "gerentes",
+    component: Gerentes,
+  },
+  {
+    path: "/cadastre-se",
+    name: "usuario.novo",
+    component: NovoUsuario,
+  },
+];
 
 const router = new VueRouter({
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
