ec8e1b0c2a223f59458d9e75c5219608d99f014c
Jhonny Ribeiro <jaribeiro32@gmail.com>
2022-04-07 05:19:15 -0300

    Nav bar as component

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,17 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand-lg navbar-bytebank">
-      <a class="navbar-brand" href="#">ByteBank</a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <router-link class="nav-link" to="/">Home</router-link>
-          </li>
-          <li class="nav-item">
-            <router-link to="/gerentes" class="nav-link">
-              Gerentes
-            </router-link>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link" @click.stop.prevent="efetarLogout"
-              >Sair</a
-            >
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <NavBar></NavBar>
     <router-view />
   </div>
 </template>
-<script >
+
+<script>
+import NavBar from "@/components/NavBar.vue";
 export default {
-  data() {
-    return {};
-  },
-  methods: {
-    efetarLogout() {
-      localStorage.removeItem("token");
-      this.$router.push({ name: "usuario.entrar" });
-    },
+  components: {
+    NavBar,
   },
 };
 </script>
 
-<style>
-.navbar {
-  background: #27ae60;
-}
-.navbar-bytebank a {
-  color: #fff;
-}
-.navbar-bytebank a:hover {
-  color: #000;
-}
-</style>
+
