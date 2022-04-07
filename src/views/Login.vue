e6c29062c133e27b7b25521e356819324f0798f3
Jhonny Ribeiro <jaribeiro32@gmail.com>
2022-04-07 05:49:19 -0300

    refactor local storage

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -47,7 +47,13 @@ export default {
         .post("auth/login", this.usuario)
         .then((response) => {
           console.log(response);
-          localStorage.setItem("token", response.data.access_token);
+          // localStorage.setItem("token", response.data.access_token);
+          // this.$store.token = response.data.access_token;
+          // this.$store.usuario = response.data.user;
+          this.$store.commit("DEFINIR_USUARIO_LOGADO", {
+            token: response.data.access_token,
+            usuario: response.data.user,
+          });
           this.$router.push({ name: "gerentes" });
         })
         .catch((error) => {
