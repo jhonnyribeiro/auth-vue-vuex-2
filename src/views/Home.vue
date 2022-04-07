451841639d235122c0105ea4ddf279cba9d961ce
Jhonny Ribeiro <jaribeiro32@gmail.com>
2022-04-07 06:17:36 -0300

    logout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="container">
     <div class="jumbotron mt-2">
       <h1 class="display-4">Bem-vindo!</h1>
+      <button class="btn btn-danger" @click="efetarLogout()">Sair</button>
       <p>Aplição disponível para listagem dos gerentes do nosso banco.</p>
     </div>
   </div>
@@ -9,6 +10,13 @@
 
 <script>
 export default {
-  name: 'home'
-}
+  name: "home",
+  methods: {
+    efetarLogout() {
+      // localStorage.removeItem("token");
+      this.$store.commit("DESLOGAR_USUARIO");
+      this.$router.push({ name: "usuario.entrar" });
+    },
+  },
+};
 </script>
